0b4242a280170e7597561df5fba7905d492cb638
Broccoli Boy <david@Davids-MacBook-Air.local>
2018-01-25 19:59:45 -0800

    swift lessons

---
+++ udacity playground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+let theTruth = "String literal"
+
+var emptyString = ""
+
+let stringWithPotential = String()
+
+for character in theTruth.characters { //DOT SYNTAX
+    print("\(character)")
+}
